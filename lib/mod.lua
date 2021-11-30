901ff6cfdf78bd890b5e11eea0c90cf899e0ec93
stvnrlly <stvnrlly@stvnrlly.com>
2021-11-29 21:10:27 -0500

    new display format

--- lib/mod.lua
+++ lib/mod.lua
@@ -4,11 +4,15 @@ local strategies = require 'oblique/strategies'
 
 local strategy = ""
 
-local function draw_strategy()
-  strategy = strategies[ math.random( #strategies ) ]
-  return strategy
-end
+-- The longest card is 5 lines on the Norns display
+local param_strats = {"","","","",""}
 
+-- Whether the strategy is unfurled in the params
+local oblique_state = {
+  show=false
+}
+
+-- utilities
 local function split_string(str)
   local split = {}
   
@@ -20,7 +24,6 @@ local function split_string(str)
 end
 
 local function build_lines(tbl)
-  local strats = {}
   local index = 1
   local line = "" .. table.remove(tbl, 1)
   
@@ -28,57 +31,96 @@ local function build_lines(tbl)
     if screen.text_extents(line .. " " .. v) <= 128 then
       line = line .. " " .. v
     else
-      table.insert(strats, index, line)
+      param_strats[index] = line
       index = index + 1
       line = "" .. v
     end
   end
   
+  -- Catch the final line
   if string.len(line) >= 1 then
-    table.insert(strats, index, line)
+    param_strats[index] = line
   end
-  
-  return strats
 end
 
+local function init_strategy()
+  param_strats = {"","","","",""}
+end
 
--- Add the strategy to the params page ////////
--- It'd be nice to do this post-startup, but that breaks for some reason
-mod.hook.register("script_pre_init", "oblique setup", function ()
-  local strategy = draw_strategy()
-  local split = split_string(strategy)
-  local strats = build_lines(split)
-  
-  params:add_separator("THE CARD SAYS")
-  
-  for k,v in ipairs(strats) do
-    params:add_text("strat" .. k, v, "")
-  end
-  
-  params:add_separator()
-end)
-
-
---
--- [optional] returning a value from the module allows the mod to provide
--- library functionality to scripts via the normal lua `require` function.
---
--- NOTE: it is important for scripts to use `require` to load mod functionality
--- instead of the norns specific `include` function. using `require` ensures
--- that only one copy of the mod is loaded. if a script were to use `include`
--- new copies of the menu, hook functions, and state would be loaded replacing
--- the previous registered functions/menu each time a script was run.
---
--- here we provide a single function which allows a script to get the mod's
--- state table. using this in a script would look like:
---
--- local mod = require 'name_of_mod/lib/mod'
--- local the_state = mod.get_state()
---
-local api = {}
-
-api.get_strategy = function()
+-- strategy management functions
+local function draw_strategy()
+  strategy = strategies[ math.random( #strategies ) ]
+  print("new strategy is: "..strategy)
   return strategy
 end
 
-return api
+local function update_strats()
+  build_lines(split_string(draw_strategy()))
+  
+  for k,v in ipairs(param_strats) do
+    params:set("oblique" .. k, v, "")
+  end
+end
+
+local function toggle_display()
+  -- loop through oblique lines and flip their display
+  -- possibly could also use params.visible to get state?
+  if oblique_state["show"] then
+    -- hide
+    for k,v in ipairs(param_strats) do
+      params:hide("oblique" .. k)
+    end
+    params:hide("oblique_redraw")
+    -- set state to false
+    oblique_state["show"] = false
+    _menu.rebuild_params()
+  else
+    -- show
+    for k,v in ipairs(param_strats) do
+      params:show("oblique" .. k)
+    end
+    params:show("oblique_redraw")
+    -- set state to true
+    oblique_state["show"] = true
+    _menu.rebuild_params()
+  end
+end
+
+-- Add the strategy to the params page
+-- It'd be nice to do this post-startup, but that breaks for some reason
+mod.hook.register("script_pre_init", "oblique setup", function ()
+  init_strategy()
+  
+  params:add_trigger("oblique_show", "OBLIQUE >")
+  params:set_action("oblique_show", function()
+    toggle_display()
+  end)
+  
+  for k,v in ipairs(param_strats) do
+    params:add_text("oblique" .. k, v, "")
+  end
+  
+  update_strats()
+  
+  params:add_trigger("oblique_redraw", "          draw again")
+  params:set_action("oblique_redraw", function(x)
+    print("redrawing...")
+    init_strategy()
+    update_strats()
+    _menu.rebuild_params() 
+  end)
+  
+  if oblique_state["show"] then
+    for k,v in ipairs(param_strats) do
+      params:show("oblique" .. k)
+    end
+    params:show("oblique_redraw")
+    _menu.rebuild_params()
+  else
+    for k,v in ipairs(param_strats) do
+      params:hide("oblique" .. k)
+    end
+    params:hide("oblique_redraw")
+    _menu.rebuild_params()
+  end
+end)
