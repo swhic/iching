cadacf9bc2644771aa7b1ae47bdb8d81c4a30691
swhic <96879054+swhic@users.noreply.github.com>
2022-01-15 16:59:13 +0000

    Update mod.lua
    iching replaced oblique

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,6 +1,6 @@
 local mod = require 'core/mods'
 
-local strategies = require 'oblique/strategies'
+local strategies = require ‘iching/strategies'
 
 local strategy = ""
 
@@ -8,7 +8,7 @@ local strategy = ""
 local param_strats = {"","","","",""}
 
 -- Whether the strategy is unfurled in the params
-local oblique_state = {
+local iching_state = {iching
   show=false
 }
 
@@ -68,68 +68,68 @@ local function update_strats()
   build_lines(split_string(draw_strategy()))
   
   for k,v in ipairs(param_strats) do
-    params:set("oblique" .. k, v, "")
+    params:set("iching" .. k, v, "")
   end
 end
 
 local function toggle_display()
-  -- loop through oblique lines and flip their display
+  -- loop through iching lines and flip their display
   -- possibly could also use params.visible to get state?
-  if oblique_state["show"] then
+  if iching_state["show"] then
     -- hide
     for k,v in ipairs(param_strats) do
-      params:hide("oblique" .. k)
+      params:hide("iching" .. k)
     end
-    params:hide("oblique_redraw")
+    params:hide("iching_redraw")
     -- set state to false
-    oblique_state["show"] = false
+    iching_state["show"] = false
     _menu.rebuild_params()
   else
     -- show
     for k,v in ipairs(param_strats) do
-      params:show("oblique" .. k)
+      params:show("iching" .. k)
     end
-    params:show("oblique_redraw")
+    params:show("iching_redraw")
     -- set state to true
-    oblique_state["show"] = true
+    iching_state["show"] = true
     _menu.rebuild_params()
   end
 end
 
 -- Add the strategy to the params page
 -- It'd be nice to do this post-startup, but that breaks for some reason
-mod.hook.register("script_pre_init", "oblique setup", function ()
+mod.hook.register("script_pre_init", "iching setup", function ()
   init_strategy()
   
-  params:add_trigger("oblique_show", "OBLIQUE >")
-  params:set_action("oblique_show", function()
+  params:add_trigger("iching_show", "iching >")
+  params:set_action("iching_show", function()
     toggle_display()
   end)
   
   for k,v in ipairs(param_strats) do
-    params:add_text("oblique" .. k, v, "")
+    params:add_text("iching" .. k, v, "")
   end
   
   update_strats()
   
-  params:add_trigger("oblique_redraw", "          draw again")
-  params:set_action("oblique_redraw", function(x)
+  params:add_trigger("iching_redraw", "          draw again")
+  params:set_action("iching_redraw", function(x)
     init_strategy()
     update_strats()
     _menu.rebuild_params() 
   end)
   
-  if oblique_state["show"] then
+  if iching_state["show"] then
     for k,v in ipairs(param_strats) do
-      params:show("oblique" .. k)
+      params:show("iching" .. k)
     end
-    params:show("oblique_redraw")
+    params:show("iching_redraw")
     _menu.rebuild_params()
   else
     for k,v in ipairs(param_strats) do
-      params:hide("oblique" .. k)
+      params:hide("iching" .. k)
     end
-    params:hide("oblique_redraw")
+    params:hide("iching_redraw")
     _menu.rebuild_params()
   end
 end)
